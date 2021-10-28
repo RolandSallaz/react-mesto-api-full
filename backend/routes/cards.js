e825be238204616a038173a5c9f7a7760b810688
RolandSallaz <machodin@yandex.ru>
2021-10-28 15:19:45 +0300

    hotfix celebrate function

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -27,21 +27,21 @@ router.post(
 );
 router.delete(
   '/:cardId', celebrate({
-    params: Joi.object.keys({
+    params: Joi.object().keys({
       cardId: Joi.string().length(24).hex().required(),
     }),
   }), deleteCard,
 );
 router.put(
   '/:cardId/likes', celebrate({
-    params: Joi.object.keys({
+    params: Joi.object().keys({
       cardId: Joi.string().length(24).hex().required(),
     }),
   }), likeCard,
 );
 router.delete(
   '/:cardId/likes', celebrate({
-    params: Joi.object.keys({
+    params: Joi.object().keys({
       cardId: Joi.string().length(24).hex().required(),
     }),
   }), dislikeCard,
