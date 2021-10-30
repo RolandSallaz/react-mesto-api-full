736baf0671c4f978c80ea94583e68d4445dbac07
RolandSallaz <machodin@yandex.ru>
2021-10-31 00:21:12 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: ['https://rolandsallaz.mesto.nomoredomains.work',
+    'http://rolandsallaz.mesto.nomoredomains.work',
+    'localhost:3000'],
+}));
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
